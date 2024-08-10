9b0545a3f0bf5667b24bae7ede85fdda86e57438
nimz <nimrahanwar992@gmail.com>
2024-08-10 15:22:19 -0400

    Setup

--- cypress/e2e/Feature/MainPage.feature
+++ cypress/e2e/Feature/MainPage.feature
@@ -3,3 +3,4 @@ Feature: Visiting Tribl
     Scenario: Home Page
         Given User will navigate to Tribl page
         Then Page should have title 'Tribl'
+        And User Click Login Button
--- cypress/e2e/StepDefinition/MainPage.js
+++ cypress/e2e/StepDefinition/MainPage.js
@@ -1,14 +1,15 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
 Given("User will navigate to Tribl page", () => {
-  // cy.visit(
-  //   "https://pre-prod.tribl.app/?_vercel_share=NQK95DCxPvER41fbVTj3D2YfPIqGXfUe"
-  // );
-
+  
   cy.login("email", "password");
   cy.visit("https://pre-prod.tribl.app/");
 });
 
 Then("Page should have title 'Tribl'", () => {
   cy.title().should("eq", "Tribl");
+});
+
+And("User Click Login Button", () => {
+  cy.get('[href="/loginwithemail"] > .z-0').click();
 });
