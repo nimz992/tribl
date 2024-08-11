92f889f96b50824c08a8e1271bd11607276a230a
nimz992 <nimrahanwar992@gmail.com>
2024-08-11 10:47:20 -0700

    Tribl Add Link

--- cypress/e2e/Feature/MainPage.feature
+++ cypress/e2e/Feature/MainPage.feature
@@ -1,8 +1,19 @@
 Feature: Visiting Tribl
 
-    Scenario: Home Page
+    Scenario: User Log-in
         Given User will navigate to Tribl page
         Then Page should have title 'Tribl'
         And User Click Login Button
         And User type email and passowrd
         Then User Logged-in
+
+    Scenario: Add Links
+        Given User will navigate to Tribl page
+        Then Page should have title 'Tribl'
+        And User Click Login Button
+        And User type email and passowrd
+        Then User Logged-in
+        Then User clicks on Add block
+        Then User click on link
+        Then User add random link
+        Then User click on Add button
--- cypress/e2e/StepDefinition/MainPage.js
+++ cypress/e2e/StepDefinition/MainPage.js
@@ -2,8 +2,10 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
 Given("User will navigate to Tribl page", () => {
   cy.viewport("macbook-15");
-  cy.login("email", "password");
-  cy.visit("https://pre-prod.tribl.app/");
+  // cy.login("email", "password");
+  cy.visit("https://www.tribl.app/");
+  cy.get(".iubenda-cs-accept-btn").click();
+  cy.wait(5000);
 });
 
 Then("Page should have title 'Tribl'", () => {
@@ -22,8 +24,31 @@ And("User type email and passowrd", () => {
     "body > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > form:nth-child(3) > div:nth-child(2) > input:nth-child(1)"
   ).type("Sanaibex123@");
 });
+
 Then("User Logged-in", () => {
   cy.get(
     ".p-8 > .text-center > :nth-child(1) > form > :nth-child(4) > .text-base"
   ).click();
+});
+
+Then("User clicks on Add block", () => {
+  cy.get(
+    "button[class='z-0 group relative inline-flex items-center justify-center box-border appearance-none select-none whitespace-nowrap font-normal subpixel-antialiased overflow-hidden tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 px-unit-4 min-w-unit-20 gap-unit-2 rounded-full w-full [&>svg]:max-w-[theme(spacing.unit-8)] data-[pressed=true]:scale-[0.97] transition-transform-colors-opacity motion-reduce:transition-none bg-primary text-primary-foreground data-[hover=true]:opacity-hover text-medium md:max-w-none md:p-0 h-[48px]']",
+    { timeout: 10000 }
+  ).click();
+});
+
+Then("User click on link", () => {
+  cy.get(
+    "body main div div div div div:nth-child(1) div:nth-child(1) div:nth-child(1)"
+  ).click();
+});
+
+Then("User add random link", () => {
+  // cy.get("input[id='react-aria8693945485-:rf:']",{timeout:10000}).type("www.google.com");
+  cy.get("input").type("www.google.com");
+});
+
+Then("User click on Add button", () => {
+  cy.get("div[class='w-[100px] false'] button[type='button']").click();
 });
